f8bee73b7aac194158ef90cf24866fa039e1ec5c
Lisa <lisa_martinez@dusty.tamiu.edu>
2018-01-31 15:39:14 -0600

    Splash screen (android)

--- src/shared/baseurl.ts
+++ src/shared/baseurl.ts
@@ -1,2 +1,2 @@
-//export const baseURL = 'http://192.168.15.5:3000/';
-export const baseURL = 'http://localhost:3000/';
+export const baseURL = 'http://192.168.0.23:3000/';
+//export const baseURL = 'http://localhost:3000/';
