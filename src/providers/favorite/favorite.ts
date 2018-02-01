914a0b26ceac6de360f96933314452d3128d94f3
Lisa <lisa_martinez@dusty.tamiu.edu>
2018-01-31 21:04:20 -0600

    Local Notifications

--- src/providers/favorite/favorite.ts
+++ src/providers/favorite/favorite.ts
@@ -5,6 +5,7 @@ import { Storage } from '@ionic/storage';
 
 import { Dish } from '../../shared/dish';
 import { DishProvider } from '../dish/dish';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 import 'rxjs/add/operator/map';
 
@@ -21,7 +22,8 @@ export class FavoriteProvider {
 
   constructor(public http: Http,
   private dishservice: DishProvider,
-  private storage: Storage) {
+  private storage: Storage,
+  private localNotifications: LocalNotifications) {
     console.log('Hello FavoriteProvider Provider');
     this.favorites = [];
 
@@ -36,10 +38,19 @@ export class FavoriteProvider {
   }
 
   addFavorite(id: number): boolean {
-    if (!this.isFavorite(id))
+    if (!this.isFavorite(id)){
       this.favorites.push(id);
-      this.storage.set('favorites', this.favorites)
+      this.storage.set('favorites', this.favorites);
+
+      this.localNotifications.schedule({
+        id: id,
+        text: 'Dish ' + id + ' added as a favorite successfully'
+      });
+
+    }
     console.log('favorites', this.favorites);
+
+
     return true;
   }
 
